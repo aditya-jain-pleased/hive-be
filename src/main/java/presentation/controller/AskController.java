d4eead67e5a5ebb80155e1040a8400dda2e0e54d
Aditya Jain <aditya.jain@pleased.com>
2025-11-28 15:03:04 +0530

    week 1

---
+++ src/main/java/presentation/controller/AskController.java
@@ -0,0 +1,12 @@
+package presentation.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AskController {
+    @PostMapping("/ask")
+    public void ask() {
+
+    }
+}
